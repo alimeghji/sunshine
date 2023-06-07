8b82654c64dea95bb0c06e79776fe2f0e543f1d9
Ali Meghji <101437447@georgebrown.ca>
2023-06-06 20:33:44 -0400

    .

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,10 +8,30 @@
 class Agent
 {
 public:
-    Vector2 lineStart;
-    Vector2 lineEnd;
     Vector2 circlePosition;
     float circleRadius;
+    float circleRotation;
+    Vector2 lineStartLeft;
+    Vector2 lineStartRight;
+    Vector2 lineEndLeft;
+    Vector2 lineEndRight;
+
+    Agent()
+    {
+        circleRotation = 0.0f;
+    }
+};
+
+class Obstacle
+{
+public:
+    Vector2 position;
+    float radius;
+
+    void Update()
+    {
+        position = GetMousePosition();
+    }
 };
 
 bool CheckCollisionLineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
@@ -25,24 +45,57 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ali's Lab 3");
     SetTargetFPS(120);
 
-    Vector2 obstaclePosition = { (float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2 };
     const float obstacleRadius = 40.0f;
 
     Agent agent1;
     agent1.circlePosition = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
     agent1.circleRadius = 40.0f;
 
+    Obstacle obstacle1;
+    obstacle1.radius = obstacleRadius;
+
     while (!WindowShouldClose())
     {
-        obstaclePosition = GetMousePosition();
-        HideCursor();
-        DrawCircleV(obstaclePosition, obstacleRadius, RED);
+        obstacle1.Update();
 
+        Vector2 direction = Normalize(Subtract(obstacle1.position, agent1.circlePosition));
+        agent1.circleRotation = atan2f(direction.y, direction.x) * RAD2DEG;  // Calculate the rotation angle of the Agent
+
+        Vector2 directionLeft = Rotate(direction, -30.0f + agent1.circleRotation);  // Rotate the direction by the Agent's rotation
+        Vector2 directionRight = Rotate(direction, 30.0f + agent1.circleRotation);  // Rotate the direction by the Agent's rotation
+
+        agent1.lineStartLeft = agent1.circlePosition;
+        agent1.lineStartRight = agent1.circlePosition;
+
+        agent1.lineEndLeft = Add(agent1.lineStartLeft, Scale(Normalize(directionLeft), agent1.circleRadius * 2));
+        agent1.lineEndRight = Add(agent1.lineStartRight, Scale(Normalize(directionRight), agent1.circleRadius * 2));
+
+        DrawCircleV(obstacle1.position, obstacleRadius, RED);
         DrawCircleV(agent1.circlePosition, agent1.circleRadius, BLUE);
+
+        // Drawing lines relative to the Agent's rotation
+        DrawLineEx(agent1.lineStartLeft, agent1.lineEndLeft, 2.0f, GREEN);
+        DrawLineEx(agent1.lineStartRight, agent1.lineEndRight, 2.0f, GREEN);
+
+        // Check collision with lines
+        bool collisionLeft = CheckCollisionLineCircle(agent1.lineStartLeft, agent1.lineEndLeft, obstacle1.position, obstacle1.radius);
+        bool collisionRight = CheckCollisionLineCircle(agent1.lineStartRight, agent1.lineEndRight, obstacle1.position, obstacle1.radius);
+
+        // Handle collision
+        if (collisionLeft)
+        {
+            // Turn right
+            agent1.circleRotation += 1.0f;
+        }
+        else if (collisionRight)
+        {
+            // Turn left
+            agent1.circleRotation -= 1.0f;
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-       
+
         EndDrawing();
     }
 
