49e5646a65580eae63763008f8d284912b9c42ea
Ali Meghji <101437447@georgebrown.ca>
2023-06-07 20:05:02 -0400

    updated

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -13,12 +13,37 @@ public:
     float circleRotation;
     Vector2 lineStartLeft;
     Vector2 lineStartRight;
+    Vector2 lineStartFrontLeft;
+    Vector2 lineStartFrontRight;
     Vector2 lineEndLeft;
     Vector2 lineEndRight;
+    Vector2 lineEndFrontLeft;
+    Vector2 lineEndFrontRight;
 
     Agent()
     {
         circleRotation = 0.0f;
+    }
+
+    void AvoidObstacle(const Vector2& obstaclePosition, bool collisionLeft, bool collisionRight, bool collisionFrontLeft, bool collisionFrontRight)
+    {
+        float rotationSpeed = 2.0f;
+        float moveSpeed = 2.0f;
+
+        if (collisionLeft || collisionFrontLeft)
+        {
+            // Turn right
+            circleRotation += rotationSpeed;
+        }
+        else if (collisionRight || collisionFrontRight)
+        {
+            // Turn left
+            circleRotation -= rotationSpeed;
+        }
+
+        // Move forward
+        circlePosition.x += cosf(circleRotation * DEG2RAD) * moveSpeed;
+        circlePosition.y += sinf(circleRotation * DEG2RAD) * moveSpeed;
     }
 };
 
@@ -31,6 +56,17 @@ public:
     void Update()
     {
         position = GetMousePosition();
+    }
+
+    bool CheckCollisionWithPoint(Vector2 point)
+    {
+        return (Distance(position, point) <= radius);
+    }
+
+    bool CheckCollisionWithLine(Vector2 lineStart, Vector2 lineEnd)
+    {
+        Vector2 nearest = NearestPoint(lineStart, lineEnd, position);
+        return Distance(nearest, position) <= radius;
     }
 };
 
@@ -54,47 +90,50 @@ int main(void)
     Obstacle obstacle1;
     obstacle1.radius = obstacleRadius;
 
+    bool collisionLeft = false;
+    bool collisionRight = false;
+    bool collisionFrontLeft = false;
+    bool collisionFrontRight = false;
+
     while (!WindowShouldClose())
     {
         obstacle1.Update();
 
         Vector2 direction = Normalize(Subtract(obstacle1.position, agent1.circlePosition));
-        agent1.circleRotation = atan2f(direction.y, direction.x) * RAD2DEG;  // Calculate the rotation angle of the Agent
+        agent1.circleRotation = atan2f(direction.y, direction.x) * RAD2DEG;
 
-        Vector2 directionLeft = Rotate(direction, -30.0f + agent1.circleRotation);  // Rotate the direction by the Agent's rotation
-        Vector2 directionRight = Rotate(direction, 30.0f + agent1.circleRotation);  // Rotate the direction by the Agent's rotation
+        Vector2 directionLeft = Rotate(direction, -45.0f);
+        Vector2 directionRight = Rotate(direction, 45.0f);
+        Vector2 directionFrontLeft = Rotate(direction, 270.0f);
+        Vector2 directionFrontRight = Rotate(direction, -270.0f);
 
         agent1.lineStartLeft = agent1.circlePosition;
         agent1.lineStartRight = agent1.circlePosition;
+        agent1.lineStartFrontLeft = agent1.circlePosition;
+        agent1.lineStartFrontRight = agent1.circlePosition;
 
         agent1.lineEndLeft = Add(agent1.lineStartLeft, Scale(Normalize(directionLeft), agent1.circleRadius * 2));
         agent1.lineEndRight = Add(agent1.lineStartRight, Scale(Normalize(directionRight), agent1.circleRadius * 2));
+        agent1.lineEndFrontLeft = Add(agent1.lineStartFrontLeft, Scale(Normalize(directionFrontLeft), agent1.circleRadius * 2));
+        agent1.lineEndFrontRight = Add(agent1.lineStartFrontRight, Scale(Normalize(directionFrontRight), agent1.circleRadius * 2));
+
+        collisionLeft = obstacle1.CheckCollisionWithLine(agent1.lineStartLeft, agent1.lineEndLeft);
+        collisionRight = obstacle1.CheckCollisionWithLine(agent1.lineStartRight, agent1.lineEndRight);
+        collisionFrontLeft = obstacle1.CheckCollisionWithLine(agent1.lineStartFrontLeft, agent1.lineEndFrontLeft);
+        collisionFrontRight = obstacle1.CheckCollisionWithLine(agent1.lineStartFrontRight, agent1.lineEndFrontRight);
+
+        agent1.AvoidObstacle(obstacle1.position, collisionLeft, collisionRight, collisionFrontLeft, collisionFrontRight);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
         DrawCircleV(obstacle1.position, obstacleRadius, RED);
         DrawCircleV(agent1.circlePosition, agent1.circleRadius, BLUE);
 
-        // Drawing lines relative to the Agent's rotation
-        DrawLineEx(agent1.lineStartLeft, agent1.lineEndLeft, 2.0f, GREEN);
-        DrawLineEx(agent1.lineStartRight, agent1.lineEndRight, 2.0f, GREEN);
-
-        // Check collision with lines
-        bool collisionLeft = CheckCollisionLineCircle(agent1.lineStartLeft, agent1.lineEndLeft, obstacle1.position, obstacle1.radius);
-        bool collisionRight = CheckCollisionLineCircle(agent1.lineStartRight, agent1.lineEndRight, obstacle1.position, obstacle1.radius);
-
-        // Handle collision
-        if (collisionLeft)
-        {
-            // Turn right
-            agent1.circleRotation += 1.0f;
-        }
-        else if (collisionRight)
-        {
-            // Turn left
-            agent1.circleRotation -= 1.0f;
-        }
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+        DrawLineEx(agent1.lineStartLeft, agent1.lineEndLeft, 2.0f, collisionLeft ? RED : GREEN);
+        DrawLineEx(agent1.lineStartRight, agent1.lineEndRight, 2.0f, collisionRight ? RED : GREEN);
+        DrawLineEx(agent1.lineStartFrontLeft, agent1.lineEndFrontLeft, 2.0f, collisionFrontLeft ? RED : GREEN);
+        DrawLineEx(agent1.lineStartFrontRight, agent1.lineEndFrontRight, 2.0f, collisionFrontRight ? RED : GREEN);
 
         EndDrawing();
     }
@@ -102,31 +141,3 @@ int main(void)
     CloseWindow();
     return 0;
 }
-
-//float angle = atan2(direction.y, direction.x) * RAD2DEG;
-        //float whiskerAngle = angle + 15;
-        //angle = angle + 360;
-        //angle = fmodf(angle, 360.0f);
-        //whiskerAngle = fmodf(whiskerAngle, 360.0f);
-
-        ////to create a vector from an angle use cos and sin
-        //Vector2 whiskerLeft = Vector2{ cosf(whiskerAngle * DEG2RAD), sinf(whiskerAngle * DEG2RAD) };
-
-        //float distanceFromObstacleToPoint = Distance(nearestPointC, obstaclePosition)
-        // bool rightCollision = (distanceFromObstacleToPoint <= obstacleRadius)
-
-//class Agent
-//{
-//public:
-//    Vector2 lineStart;
-//    Vector2 lineEnd;
-//    Vector2 circlePosition;
-//    float circleRadius;
-//};
-//
-//class Obstacle
-//{
-//public:
-//    Vector2 obstaclePosition = { (float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT / 2 };
-//    const float obstacleRadius = 40.0f;
-//};
